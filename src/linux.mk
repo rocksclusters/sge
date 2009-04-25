b247b38df0520613264fd378c92e05bca1c0ad4d
Greg Bruno <greg.bruno@gmail.com>
2009-04-25 03:56:01 +0000

    tweaks

--- src/linux.mk
+++ src/linux.mk
@@ -1,1 +1,5 @@
-SRCDIRS = `find . -type d -maxdepth 1 -not -name CVS -not -name .`
+SRCDIRS = `find . -type d -maxdepth 1 \
+	-not -name CVS \
+	-not -name globus-sge \
+	-not -name sun_sge \
+	-not -name .`
