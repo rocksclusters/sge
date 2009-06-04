1eeea4983c55eef6458a485f79181033e46ae614
Anoop Rajendra <anoop.rajendra@gmail.com>
2009-06-04 01:40:05 +0000

    Bug fixes for the latest version of SGE

--- src/sun_sge/sge_exec_setup.sh
+++ src/sun_sge/sge_exec_setup.sh
@@ -5,19 +5,20 @@ SGE_CONFIG_FILE=$SGE_ROOT/util/install_modules/sge_configuration.conf
 
 case $1 in
 '-c')
-	mkdir -p $SGE_ROOT/default/spool/qmaster
+	mkdir -p $SGE_ROOT/default/spool
 	chown -R sge:sge $SGE_ROOT
-	(\
-		cd $SGE_ROOT;	\
-		./inst_sge -x -noremote	\
-		-auto $SGE_CONFIG_FILE; \
-	)
-	cp $SGE_ROOT/default/common/sgeexecd /lib/svc/method/sgeexecd
-	chmod a+x /lib/svc/method/sgeexecd
+	cp $SGE_ROOT/default/common/sgeexecd \
+		/lib/svc/method/sgeexecd
 	rm -rf /etc/rc2.d/K02sgeexecd
 	rm -rf /etc/rc2.d/S96sgeexecd
 	rm -rf /etc/init.d/sgeexecd
-	/lib/svc/method/sgeexecd stop
+	chmod a+x /lib/svc/method/sgeexecd
+	(				\
+		cd $SGE_ROOT;		\
+		./inst_sge -x -noremote	-nosmf \
+		-auto $SGE_CONFIG_FILE; \
+	)
+	/lib/svc/method/sgeexecd -nosmf stop
 	;;
 
 '-u')
