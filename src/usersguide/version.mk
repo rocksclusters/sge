bba6e5f65c1adbdf80239e66ff23164eedbb6494
Mason Katz <mason.katz@gmail.com>
2011-02-14 22:14:17 +0000

    - Build Env   - ROLLNAME defaults to ROLL   - Fixes for docbook - All userguides build again - All userguides are now NOARCH rpms

--- src/usersguide/version.mk
+++ src/usersguide/version.mk
@@ -1,11 +1,11 @@
-ROLL	= sge
 NAME    = roll-$(ROLL)-usersguide
 RELEASE = 0
+RPM.ARCH = noarch
 
 SUMMARY_COMPATIBLE      = $(VERSION)
 SUMMARY_MAINTAINER      = Rocks Group
 SUMMARY_ARCHITECTURE    = i386, x86_64
 
-ROLL_REQUIRES           = base hpc kernel os1 os2 sge
+ROLL_REQUIRES           = base kernel os
 ROLL_CONFLICTS          = pbs
 
