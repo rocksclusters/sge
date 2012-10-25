0f4969b572fd41f89f9831aa3258cd47ba7cf6a1
Luca Clementi <luca.clementi@gmail.com>
2012-10-25 00:04:54 +0000

    simple submission test for SGE

---
+++ tests/scripts/job.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+
+echo running on `hostname`
+sleep 5
+touch testsge
+echo done running on `hostname`
+
---
+++ tests/test_sge_submit.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# Test vm machines
+#
+# prerequisite 2 vm-container already up and running
+#
+# no virtual compute already installed
+#
+# maxrun time 2 hours
+
+function reportError {
+	echo $1
+	exit -1
+}
+
+
+function Pause {
+	echo $1 press any key to continue execution
+	OLDCONFIG=`stty -g`
+	stty -icanon -echo min 1 time 0
+	dd count=1 2>/dev/null
+	stty $OLDCONFIG
+}
+
+
+TESTUSER=testsge
+
+#echo creating user test
+#useradd -m $TESTUSER
+#rocks sync users
+
+while [ true ]; do
+	sleep 5
+	if [ -d /home/$TESTUSER/ ];then
+		break
+	fi
+done
+
+
+cp -p scripts/job.sh /home/$TESTUSER/
+chown $TESTUSER /home/$TESTUSER/job.sh
+su - $TESTUSER -c "qsub -sync y job.sh " || reportError "Unable to run condor_submit"
+
+if [ ! -f /home/$TESTUSER/testsge ];then
+	reportError "Condor job did not create the test file."
+else
+        echo Sucesfully tested sge submission
+        exit
+fi
+
+ 
