9ac5fbc6b38fa6e2f701f28b37ed78eb5d0e696f
Philip Papadopulos <ppapadopoulos@ucsd.edu>
2017-10-01 18:39:56 -0400

    RPM.FILES

--- src/rocks-sge/version.mk
+++ src/rocks-sge/version.mk
@@ -1,4 +1,9 @@
 NAME		= rocks-sge
 #VERSION		= V60u6
 RELEASE		= 2
-
+PKGROOT = /opt/gridengine
+RPM.FILES = \
+/etc/profile.d/* \n\
+$(PKGROOT)/util/install_modules/* \n \
+$(PKGROOT)/bin/* \n \
+$(PKGROOT)/mpi/* 
