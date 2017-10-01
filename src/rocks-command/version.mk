27a45d31876de94b23b33c05f61a1197c0705f93
Philip Papadopulos <ppapadopoulos@ucsd.edu>
2017-10-01 17:57:13 -0400

    RPM.FILES updated

--- src/rocks-command/version.mk
+++ src/rocks-command/version.mk
@@ -1,2 +1,6 @@
 NAME	= rocks-command-sge
 RELEASE	= 0
+RPM.FILES = $(PY.ROCKS)/rocks/commands/remove/host/* \n\
+$(PY.ROCKS)/rocks/commands/report/sge \n\
+$(PY.ROCKS)/rocks/commands/report/host/sge 
+
